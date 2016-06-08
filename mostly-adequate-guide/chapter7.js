9d047fcb6ff170675dfc2c5e3d09b5060a518290
zhanglin <zhanglin@ifeng.com>
2016-06-08 17:37:37 +0800

    study

--- mostly-adequate-guide/chapter7.js
+++ mostly-adequate-guide/chapter7.js
@@ -12,8 +12,23 @@ var concat = R.curry(function (a, b) {
     return a + b;
 });
 
+var split = R.curry(function (str, xs) {
+    return xs.split(str);
+});
+
+var last = function (xs) {
+    return xs[xs.length - 1];
+};
+
+var eq = R.curry(function (key, value) {
+    return key === value;
+});
 var id = function (x) {
     return x;
+};
+
+var head = function (xs) {
+    return xs[0]
 };
 
 var Container = function (x) {
@@ -112,11 +127,11 @@ Right.prototype.map = function (f) {
     return Right.of(f(this.__value));
 };
 
-console.log(Right.of('rain').map(function(str) {return 'b' + str}));
-console.log(Left.of('rain').map(function(str) {return 'b' + str}));
+// console.log(Right.of('rain').map(function(str) {return 'b' + str}));
+// console.log(Left.of('rain').map(function(str) {return 'b' + str}));
 
-console.log(Right.of({host: 'localhost', port: 80}).map(R.prop('host')));
-console.log(Left.of('rolls eyes...').map(R.prop('host')));
+// console.log(Right.of({host: 'localhost', port: 80}).map(R.prop('host')));
+// console.log(Left.of('rolls eyes...').map(R.prop('host')));
 
 var getAge = R.curry(function (now, user) {
     var birthdate = moment(user.birthdate, 'YYYY-MM-DD');
@@ -126,12 +141,12 @@ var getAge = R.curry(function (now, user) {
     return Right.of(now.diff(birthdate, 'years'));
 });
 
-console.log(getAge(moment(), {birthdate: '2005-12-12'}));
-console.log(getAge(moment(), {birthdate: '20010104'}));
+// console.log(getAge(moment(), {birthdate: '2005-12-12'}));
+// console.log(getAge(moment(), {birthdate: '20010104'}));
 
 var fortune = R.compose(concat('if you survive, you will be '), add(1));
 var zoltar = R.compose(map(console.log.bind(console)), map(fortune), getAge(moment()));
-console.log(zoltar({birthdate: 'xxxx'}));
+// console.log(zoltar({birthdate: 'xxxx'}));
 
 var either = R.curry(function (f, g, e) {
     switch (e.constructor) {
@@ -141,6 +156,43 @@ var either = R.curry(function (f, g, e) {
 });
 
 var zoltar = R.compose(console.log.bind(console), either(id, fortune), getAge(moment()));
-console.log(zoltar({birthdate: '2005-12-12'}));
-console.log(zoltar({birthdate: 'xxxxx'}));
+// console.log(zoltar({birthdate: '2005-12-12'}));
+// console.log(zoltar({birthdate: 'xxxxx'}));
+
+var IO = function (f) {
+    this.__value = f;
+};
+
+IO.of = function (x) {
+    return new IO(function () {
+        return x;
+    });
+};
+
+IO.prototype.map = function (f) {
+    return new IO(R.compose(f, this.__value));
+};
+
+var io_window = IO.of(window);
+console.log(io_window.map(function (win) { console.log(win.innerWidth); return win.innerWidth}));
+console.log(io_window.map(R.prop('location')).map(R.prop('href')).map(split('/')));
+
+var $ = function (selector) {
+    return new IO(function () {
+        return document.querySelectorAll(selector);
+    });
+};
+
+$('#myDiv').map(head).map(function (div) {return div.innerHTML;});
+
+var url = new IO(function () {return window.location.href;});
+var toPairs = R.compose(R.map(split('=')), split('&'));
+var params = R.compose(toPairs, last, split('?'));
+var findParam = function (key) {
+    return map(R.compose(map(last), Maybe.of, head, R.filter(R.compose(eq(key), head)), params), url);
+};
+
+console.log(findParam('searchTerm').__value());
+
+
 
